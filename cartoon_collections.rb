bb77a147fa1321836f9029181b1b8f3e556ff721
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-11 02:00:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{ |value, index| puts "#{index + 1} #{value}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|value| "#{value.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.find{|item| item.length > 4}
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each{|item|
+  if array.include?(item)
+    return item
+  end  
+  }
 end
