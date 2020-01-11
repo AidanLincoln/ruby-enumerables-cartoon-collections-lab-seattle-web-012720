fdd9071638c8d0740241a68652baed108c6ff6c1
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-11 02:18:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,12 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  cheese = ""
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |item|
   if array.include?(item)
-    return item
+    cheese = item
+    return cheese
   end
   end
 end
